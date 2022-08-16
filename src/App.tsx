554e34ac75aef0b2488f74787e1c95501f17e372
HoseinBayati <byt.hossein.bayati@gmail.com>
2022-08-16 16:30:07 +0430

    something

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Loginpage from './Login';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
